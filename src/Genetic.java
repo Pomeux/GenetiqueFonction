b81eb528e83d804a183df80869a6b7c3694523ce
Leo <leopldu45@gmail.com>
2026-01-16 21:12:24 +0100

    first commit

---
+++ src/Genetic.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Genetic {
+
+    private double f(double x) {
+        return x*Math.sin(Math.PI*10+x)+1.0;
+    }
+
+    /**
+     * @author Léo Planque
+     * @param
+     * @return retourne le nombre d'éléments dans un intervalle
+     * */
+
+
+    //TODO faire les test avec Junit pour nbVariable
+    private int nbVariable(int[] intervalle) {
+        if( intervalle.length!=2)
+            throw new IllegalArgumentException("");
+
+        int a=intervalle[0];
+        int b=intervalle[1];
+
+        if(a>b)
+            throw new IllegalArgumentException("");
+
+        if(a*b>=0) {
+            return Math.abs(b-a);
+        } else {
+            return Math.abs(a)+b;
+        }
+
+
+    }
+    private ArrayList<Integer> represent_solution(int[] intervalle,int precision) {
+
+        int size=precision*nbVariable(intervalle);
+        double a=(double) size;
+
+        int nbBit=Math.log(1.0);
+
+        return null;
+    }
+}
