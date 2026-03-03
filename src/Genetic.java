6ea672afdd96da4a49aae168eda061054eaf9b6a
Leo <leopldu45@gmail.com>
2026-03-03 23:27:22 +0100

    add function

--- src/Genetic.java
+++ src/Genetic.java
@@ -1,9 +1,30 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Genetic {
 
-    private double f(double x) {
-        return x*Math.sin(Math.PI*10+x)+1.0;
+    private int precission;
+
+    public static double f(double x) {
+
+        return (x*Math.sin(Math.PI*10+x)+1.0);
+    }
+    public static double floorPrecissionDecimal(double x, int precission) {
+        double p=Math.pow(10.0,precission);
+        return Math.floor(x*p)/p;
+    }
+    //ajouté exception+faire classe intervalle
+    public static double corresponding_number(ArrayList<Integer> binary,Intervalle<Integer> intervalle,int precision) {
+
+
+        double sum=0.0;
+        for(int i=0;i<binary.size();i++) {
+            sum+=Math.pow(2.0,i)*binary.get(i);
+        }
+        double precission=Math.pow(10.0,(double) precision);
+        return floorPrecissionDecimal(((-intervalle.getInf()+
+                sum*intervalle.getSup()/(Math.pow(2.0,binary.size())-1))),precision);
     }
 
     /**
@@ -14,12 +35,13 @@ public class Genetic {
 
 
     //TODO faire les test avec Junit pour nbVariable
-    private int nbVariable(int[] intervalle) {
-        if( intervalle.length!=2)
-            throw new IllegalArgumentException("");
+    //modif pour que ça soit n'importe quelle double en inf et sup+ que ça retourne bien le nb de variable même si c'est des double
+    //modif le nom des variables
+    //rajouter size en attribut
+    private static int nbVariable(Intervalle<Integer> intervalle) {
 
-        int a=intervalle[0];
-        int b=intervalle[1];
+        int a=intervalle.getInf();
+        int b=intervalle.getSup();
 
         if(a>b)
             throw new IllegalArgumentException("");
@@ -32,18 +54,34 @@ public class Genetic {
 
 
     }
-    private ArrayList<Integer> represent_solution(int[] intervalle,int precision) {
+    private ArrayList<ArrayList<Integer>> generate_solution(Intervalle<Integer> intervalle,int precision) {
 
         int size=precision*nbVariable(intervalle);
 
 
-        int nbBit=(int) Math.floor(Math.log((double) size));
+        int nbBit=(int) Math.ceil(Math.log((double) size)/Math.log(2.0));
+
+        ArrayList<ArrayList<Integer>> tab=new ArrayList<ArrayList<Integer>>(size);
 
         for(int i=0;i<size;i++) {
-            
+            ArrayList<Integer> tab_tempo=new ArrayList<Integer>(nbBit);
 
+            for(int j=0;j<nbBit;j++) {
+                int rand = (int) (Math.random() * 2);
+
+                tab_tempo.add(rand);
+
+            }
+            tab.add(tab_tempo);
         }
 
-        return null;
+
+        return tab;
+    }
+
+    public static void one_crossover(List<Integer> parent1,List<Integer> parent2) {
+        int indexCross=(int) (Math.random()*(parent1.size())-1);
+
+        faire fusion de list voir avec les itératurs si on peut pas faire en O(1) avec des pointeurs peut-être
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int size=2*10;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        int nbBit=(int) Math.ceil(Math.log((double) size)/Math.log(2.0));
+
+        ArrayList<ArrayList<Integer>> tab=new ArrayList<ArrayList<Integer>>(size);
+
+        for(int i=0;i<size;i++) {
+            ArrayList<Integer> tab_tempo=new ArrayList<Integer>(nbBit);
+
+            for(int j=0;j<nbBit;j++) {
+                int rand = (int) (Math.random() * 2);
+
+                tab_tempo.add(rand);
+
+            }
+            tab.add(tab_tempo);
         }
+        for(int i=0;i<size;i++) {
+
+            for(int j=0;j<nbBit;j++) {
+                System.out.print(tab.get(i).get(j));
+
+            }
+            double x=Genetic.corresponding_number(tab.get(i),new Intervalle<Integer>(0,2),2);
+            System.out.println(" x: "+x+" f(x): "+Genetic.floorPrecissionDecimal(Genetic.f(x),2));
+        }
+
+        ArrayList<Integer> b=new ArrayList<Integer>(10);
+
+
+        ArrayList<Integer> a=new ArrayList<Integer>(10);
+        for(int i=1;i<=10;i++) {
+            b.add(i);
+        }
+
+
+        for(int i=1;i<=10;i++) {
+            a.add(-1*i);
+        }
+        Genetic.crossover(a,b);
+
     }
+
 }
